122e7b6604d7295a9fc96f3a3038429b35a706fa
Manish Kumar Sahu <msahu300@gmail.com>
2021-07-22 13:47:22 +0530

    fix: oauth callback url

--- packages/api/controllers/auth.js
+++ packages/api/controllers/auth.js
@@ -1,13 +1,18 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../models/user");
 
+const CALLBACK_URL = !process.env.PROD
+      ? `http://localhost:4000/api/auth/google/redirect`
+      : `https://june-api.herokuapp.com/api/auth/google/redirect`;
+
+
 exports.setupGoogleOauth = (passportInstance) => {
   passportInstance.use(
     new GoogleStrategy(
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:4000/api/auth/google/redirect",
+        callbackURL: CALLBACK_URL,
         passReqToCallback: true,
       },
       async (request, accessToken, refreshToken, profile, done) => {
